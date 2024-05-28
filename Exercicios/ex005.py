922010e41ac8f50ccb666b8fa1cba5c32b6eceb9
Wellington <costawellington702@gmail.com>
2024-05-28 16:14:43 -0300

    Update ex005.py

--- Exercicios/ex005.py
+++ Exercicios/ex005.py
@@ -1,4 +1,7 @@
-n1 = int(input('Digite um número:'))
-ant = n1-1
-suc = n1+1
-print('O valor antecessor é {} e o sucessor é {}'.format(ant,suc))
+n = int(input('Digite um número:'))
+#a = n-1
+#s = n+2
+#print('Analisando o seu valor {} seu antecessor é {} e o sucessor é {}'.format(n,a,s))
+#mas haverão vezes que precisaremos utilizar dessa forma especifica então vai valer a pena criar para cada um;
+print('Analisando o valor {} seu antecessor é {} e o sucessor é {}'.format(n,(n-1),(n+1)))
+#essa é a forma utilizando apenas uma variavel, quanto menos utilizarmos variaveis, mais memoria vamos economizar.
