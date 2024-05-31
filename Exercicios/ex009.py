c85ca38c108723b90e8274978132e244c11cfd66
Wellington <costawellington702@gmail.com>
2024-05-30 22:52:05 -0300

    tabuada

---
+++ Exercicios/ex009.py
@@ -0,0 +1,13 @@
+n = int(input('Tabuada - Digite um número:'))
+v1 = n * 1
+v2 = n * 2
+v3 = n * 3
+v4 = n * 4
+v5 = n * 5
+v6 = n * 6
+v7 = n * 7
+v8 = n * 8
+v9 = n * 9
+v10 = n * 10
+
+print('-------------------\n {} x  1 = {}\n {} x  2 = {}\n {} x  3 = {}\n {} x  4 = {}\n {} x  5 = {}\n {} x  6 = {}\n {} x  7 = {}\n {} x  8 = {}\n {} x  9 = {}\n {} x 10 = {}\n -------------------'.format(n,v1,n,v2,n,v3,n,v4,n,v5,n,v6,n,v7,n,v8,n,v9,n,v10))
